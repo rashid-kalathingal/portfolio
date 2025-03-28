4c8091542308620cd617d4fe6d512e224a26c4be
rashid30111998 <kalathingalrashid89@gmail.com>
2025-03-28 20:27:39 +0300

    committ

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1600,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    },
+    chunkSizeWarningLimit: 1600,
   },
 });
